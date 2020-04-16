b510adcc29130706e752d2d2e59a071f49f8908b
Tim <t.fahrenreich@gmail.com>
2020-04-16 15:15:31 -0700

    WIP Geolocation handling/listening

--- src/listeners/location.js
+++ src/listeners/location.js
@@ -1,35 +1,61 @@
-import React, {useReducer, useState, useEffect} from 'react';
-import {Platform, Text, View, StyleSheet} from 'react-native';
+import React, {useState, useCallback} from 'react';
+
+import {Text, Button} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import * as Location from 'expo-location';
 
 const GetOutApp = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [homeLocation, setHomeLocation] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      let {status} = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+  // useEffect(() => {
+  //   (async () => {
+  //     let {status} = await Location.requestPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //     }
+  //
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //     console.log("ASD")
+  //   })();
+  // });
+
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
+  const saveHomeLocation = useCallback(() => {
+    const save = async () => {
+      const currentLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+
+      try {
+        await AsyncStorage.setItem(
+          '@home_location',
+          JSON.stringify(currentLocation.coords),
+        );
+
+        setHomeLocation(currentLocation.coords);
+      } catch (e) {
+        // saving error
       }
+    };
+    save();
+  }, []);
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  });
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  return (
-    <View>
-      <Text>{text}</Text>
-    </View>
+  return !homeLocation ? (
+    <>
+      <Button title={'Set Home Location'} onPress={saveHomeLocation} />
+    </>
+  ) : (
+    <Text>home: {JSON.stringify(homeLocation)}</Text>
   );
 };
 
